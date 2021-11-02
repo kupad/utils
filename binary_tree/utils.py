c403c7ec515639fed3952cc362ea5c4252b8b31f
kupad <dreizen@kupad.net>
2021-11-02 15:47:38 -0400

    more convenient outter insert

--- binary_tree/utils.py
+++ binary_tree/utils.py
@@ -4,21 +4,23 @@ class TreeNode:
      self.left = left
      self.right = right
 
-def insert(root, parent, val):
-    """
-    insert value into first available slot
-    """
-    if root is None:
-        if parent.left is None:
-            parent.left = TreeNode(val)
+def insert(root, val):
+    def _insert(root, parent, val):
+        """
+        insert value into first available slot
+        """
+        if root is None:
+            if parent.left is None:
+                parent.left = TreeNode(val)
+            else:
+                parent.right = TreeNode(val)
+            return
+       
+        if root.left is None:
+            _insert(root.left, root, val)
         else:
-            parent.right = TreeNode(val)
-        return
-   
-    if root.left is None:
-        insert(root.left, root, val)
-    else:
-        insert(root.right, root, val)
+            _insert(root.right, root, val)
+    _insert(root, None, val)
 
 def print_tree(root):
     """
@@ -31,21 +33,22 @@ def print_tree(root):
     print_tree(root.left)
     print_tree(root.right)
     
-def totree(L):
+def to_tree(L):
     """
     Takes a list and returns a Binary Tree.
     Not a BST! Just a binary tree in the order of the list
 
     TODO: speed up
     """
-    if len(L) == 0:
+    if not L:
         return None
+
     root = TreeNode(L[0])
     for val in L[1:]:
-        insert(root, None, val)
+        insert(root, val)
     return root
 
 if __name__ == '__main__':
-    tree = totree([1,3,2,5])
+    tree = to_tree([1,3,2,5])
     print_tree(tree)
 
